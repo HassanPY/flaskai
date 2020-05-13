91a7b5f05894fa5555574aa3481dcce19209f965
Hassan <hassan@Hassans-MacBook-Pro.local>
2020-05-13 15:23:53 +0400

    Working on if elif ... still problem

--- dashapp/dashfile.py
+++ dashapp/dashfile.py
@@ -18,6 +18,13 @@ import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
 
+import os
+import firebase_admin
+from firebase_admin import db
+import pandas as pd
+from pandas.io.json import json_normalize
+import json
+
 
 def Add_Dash(server):
     # Create Dash app.
@@ -30,11 +37,18 @@ def Add_Dash(server):
                     external_stylesheets=external_stylesheets,
                     external_scripts=external_scripts,
                     routes_pathname_prefix='/dash/')
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "flaskai/flaskai-dec7b-firebase-adminsdk-9o96s-1b92419593.json"
+    firebase_admin.initialize_app(options={'databaseURL': 'https://flaskai-dec7b.firebaseio.com'})
+
+    # Fetch Data from firebase online server
+    data = Fetch_Data_FBserver()
+    # print(data.columns.values)
 
     # Create Dash Layout
     # dash_app.layout = html.Div(id='dash-container', children=[
     #     html.H1('Hello Hassan')
     # ])
+
     dash_app.layout = html.Div(children=[
         html.H1(children='Hello Dash'),
 
@@ -73,6 +87,89 @@ def Add_Dash(server):
     # init_callbacks(dash_app)
 
     return dash_app.server
+
+
+def Fetch_Data_FBserver():
+    rootreference = 'IoT/'
+    # child = 'Robot'
+    ref = rootreference
+    iots = db.reference(ref)
+    data = iots.get()
+    data_keys = data.keys()
+    # data_values = data.values()
+
+    with open('./data/keys.csv', 'w') as f:
+
+        header = "IOT Device"+","+"Unique_Trs_ID"
+        f.write(header+"\n")
+        for key in data_keys:
+            for unique_trs_id, record in data[key].items():
+                line = key + "," + unique_trs_id
+                f.write(line+"\n")
+
+    with open('./data/details.csv', 'w') as df:
+        dheader = "unique_trs_id"+','+"Latitude"+","+"Longitude"+","+"BatteryisCharging"+"," + "Battery Percentage" + \
+            ","+"CO2"+","+"Preasure Hg"+","+"Humidity %"+"," + \
+                "Temperature C"+"," + "Speed KM"+","+"Date"+","+"Time"
+        df.write(dheader+"\n")
+
+        dline = {'unique_trs_id': '', 'lat': '', 'lon': '', 'isCharging': '', 'percentage': '',
+                 'CO2 %': '', 'Preasure Hg': '', 'Humidity %': '', 'Temperature C': '', 'Speed KM': '', 'date': '', 'time': ''}
+
+        def write_to_csv_file():
+            [df.write('{0},'.format(value)) for key, value in dline.items()]
+            df.write("\n")
+
+        for key in data_keys:
+            for unique_trs_id, record in data[key].items():
+                dline['unique_trs_id'] = unique_trs_id
+                if 'GPS' in record:
+                    dline['lat'] = record['GPS']['lat']
+                    dline['lon'] = record['GPS']['lon']
+                    write_to_csv_file()
+                elif 'Battery' in record:
+                    dline['isCharging'] = record['Battery']['isCharging']
+                    dline['percentage'] = record['Battery']['percentage']
+                    write_to_csv_file()
+                elif 'CO2 %' in record:
+                    dline['CO2 %'] = record['CO2 %']
+                    write_to_csv_file()
+                elif 'Preasure Hg' in record:
+                    dline['Preasure Hg'] = record['Preasure Hg']
+                    write_to_csv_file()
+                elif 'Humidity %' in record:
+                    dline['Humidity %'] = record['Humidity %']
+                    write_to_csv_file()
+                elif 'Temperature C' in record:
+                    dline['Temperature C'] = record['Temperature C']
+                    write_to_csv_file()
+                elif 'Speed KM' in record:
+                    dline['Speed KM'] = record['Speed KM']
+                    write_to_csv_file()
+                elif 'date' in record:
+                    dline['date'] = record['date']
+                    write_to_csv_file()
+                elif 'time' in record:
+                    dline['time'] = record['time']
+                    write_to_csv_file()
+                else:
+                    print('else is working ######')
+                # the following line is to reset dline,
+                # location of the line to be confirmed
+                dline = {'unique_trs_id': '', 'lat': '', 'lon': '', 'isCharging': '', 'percentage': '',
+                         'CO2 %': '', 'Preasure Hg': '', 'Humidity %': '', 'Temperature C': '', 'Speed KM': '', 'date': '', 'time': ''}
+
+    with open('./data/iotdata .json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+    df = pd.read_json('./data/iotdata.json')
+    # print(json_normalize(df, max_level=0))
+    # print(df.to_json(orient='split'))
+
+    # print(df.to_json(orient='index'))
+    df.to_csv('./data/iotdata.csv')
+
+    return df
 
 
 # Note this for callbacks:
